f964a8028be2b1ae1014aeb4e96ba67462f6ffe2
A M Isaac <aisaac@wccnet.edu>
2022-08-17 14:55:14 -0400

    Learn how to handle null and undefined values as related to routing

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   //selector is only needed if nesting a component in
@@ -10,12 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: String = "Product Detail";
-
+  //product: IProduct; - won't compile because is not defined.
+  //two solutions:
+  //product!:IProduct; //tells compiler will be defined later.
+  product: IProduct | undefined; //is defined as undefined until defined as IProduct
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle +=`: ${id}`;
+    this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2021",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
   }
 
 }
